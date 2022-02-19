ce3bba129244bc5786406aff42edc4268c391511
Uchiha-Shisui <aleqs.amir@gmail.com>
2022-02-19 13:47:21 +0400

    class_trait_MySql

---
+++ das81_tnayin(1)/distribution.php
@@ -0,0 +1,26 @@
+<?php
+    require_once "For_SQL.php";
+    class distribution {
+        use For_SQL;
+        public function get_to_distribute($arr, $action) {
+            $keys = [];
+            $values = [];
+            foreach($arr as $key => $value) {
+                array_push($keys, $key);
+                array_push($values, $value); 
+            }
+            switch($action) {
+                case 'insert' : $this->insertion($keys, $values);
+                    break;
+                case 'delete' : $this->del($keys, $values);
+                    break;
+                case 'select' : $this->select($keys, $values);
+                    break;
+                case 'update' : $this->update($keys, $values);
+                    break;
+            }
+        }
+    } 
+    $obj = new distribution();
+    $arr = ['email' => 'aaa2@gmail.com', 'name' => 'Alex2', 'login' => 'Kokushibo2', 'password' => '123456aa2'];
+    $obj->get_to_distribute($arr, 'select');
